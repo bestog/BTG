6d3afa64626bf8922c648e22435ee9a346793a72
bestog <info@bestog.de>
2017-07-09 18:07:03 +0200

    Travis CI fix

--- tests/Test.php
+++ tests/Test.php
@@ -2,13 +2,21 @@
 
 namespace bestog;
 
+/**
+ * Class Test
+ *
+ * @package bestog
+ */
 class Test extends \PHPUnit_Framework_TestCase {
 
+	/**
+	 *
+	 */
 	public function test_isTimestamp() {
-		$this->assertEquals(true, BTG::isTimestamp(time()));
-		$this->assertEquals(true, BTG::isTimestamp(1));
-		$this->assertEquals(false, BTG::isTimestamp('1970-01-01 01:00:00'));
-		$this->assertEquals(false, BTG::isTimestamp('12:00:00'));
+		static::assertEquals(true, BTG::isTimestamp(time()));
+		static::assertEquals(true, BTG::isTimestamp(1));
+		static::assertEquals(false, BTG::isTimestamp('1970-01-01 01:00:00'));
+		static::assertEquals(false, BTG::isTimestamp('12:00:00'));
 	}
 
 }
