1910690171c7c1233b12e580ccc8d648be7d6198
bestog <info@bestog.de>
2017-09-28 21:53:35 +0200

    Add new functions and tests.

--- src/BTG.php
+++ src/BTG.php
@@ -6,31 +6,83 @@ date_default_timezone_set('Europe/Berlin');
 
 /**
  * Class BTG
+ * Useful PHP-Functions
  *
  * @package bestog
  */
 class BTG {
 
 	/**
-	 * @param $value
+	 * Get datetime from timestamp or date
 	 *
-	 * @return false|string
+	 * @param int|boolean|string $value Date or timestamp
+	 *
+	 * @return string
 	 */
 	public static function datetime($value) {
 		if (!self::isTimestamp($value)) {
 			$value = \strtotime($value);
 		}
-
 		return date('Y-m-d H:i:s', $value);
 	}
 
 	/**
-	 * @param $value
+	 * Is the value a valid timestamp?
+	 *
+	 * @param int|string $stamp
 	 *
 	 * @return bool
 	 */
-	public static function isTimestamp($value) {
-		return is_numeric($value) && $value > 0;
+	public static function isTimestamp($stamp) {
+		return ctype_digit($stamp) && strtotime(date('Y-m-d H:i:s', $stamp)) === (int)$stamp;
+	}
+
+	/**
+	 * Format bytes to a readable size
+	 *
+	 * @param int $bytes    Bytes
+	 * @param int $decimals Decimals
+	 *
+	 * @return string
+	 */
+	public static function readableSize($bytes, $decimals = 2) {
+		$size   = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+		$factor = (int)floor((strlen($bytes) - 1) / 3);
+		return sprintf("%.{$decimals}f", $bytes / (1024 ** $factor)).$size[ $factor ];
+	}
+
+	/**
+	 * Check if value is a hex color (or shorthand hex color)
+	 *
+	 * @param string $value (example: #ff00ff or #f0f)
+	 *
+	 * @return int  (6 = #ff00ff, 3 = #f0f, -1 = invalid)
+	 */
+	public static function isHexColor($value) {
+		$check = function($value, $len) {
+			return preg_match('/^#(?:[0-9a-fA-F]{'.$len.'})$/', $value) && strlen($value) === $len + 1;
+		};
+		return $check($value, 6) ? 6 : ($check($value, 3) ? 3 : -1);
+	}
+
+	/**
+	 * Get nested array from ArrayList
+	 *
+	 * @param array  $array
+	 * @param string $nestedKey
+	 * @param mixed  $nestedValue
+	 *
+	 * @return array|null
+	 */
+	public static function getNestedArray($array, $nestedKey, $nestedValue) {
+		if (count($array) > 0) {
+			foreach ($array as $item) {
+				if ($item[ $nestedKey ] === $nestedValue) {
+					return $item;
+				}
+			}
+		}
+		return null;
 	}
 
 }
--- tests/Test.php
+++ tests/Test.php
@@ -14,14 +14,59 @@ class Test extends \PHPUnit_Framework_TestCase {
 	 */
 	public function test_isTimestamp() {
 		static::assertEquals(true, BTG::isTimestamp(time()));
-		static::assertEquals(true, BTG::isTimestamp(1));
+		static::assertEquals(true, BTG::isTimestamp('123456789'));
 		static::assertEquals(false, BTG::isTimestamp('1970-01-01 01:00:00'));
 		static::assertEquals(false, BTG::isTimestamp('12:00:00'));
 	}
 
+	/**
+	 * Test: datetime
+	 */
 	public function test_datetime() {
 		$nowTime = time();
 		static::assertEquals(date('Y-m-d H:i:s', $nowTime), BTG::datetime($nowTime));
+	}
+
+	/**
+	 * Test: readableSize
+	 */
+	public function test_readableSize() {
+		// low value and decimal precision
+		static::assertEquals('1.00B', BTG::readableSize(1));
+		static::assertEquals('1.000B', BTG::readableSize(1, 3));
+		// high value
+		static::assertEquals('9.09TB', BTG::readableSize(9999999999999));
+		// string value
+		static::assertEquals('200.00B', BTG::readableSize('200'));
+	}
+
+	/**
+	 * Test: isHexColor
+	 */
+	public function test_isHexColor() {
+		// correct hex codes
+		static::assertEquals(3, BTG::isHexColor('#f0f'));
+		static::assertEquals(6, BTG::isHexColor('#ff00ff'));
+		// wrong hex codes
+		static::assertEquals(-1, BTG::isHexColor('f0f'));
+		static::assertEquals(-1, BTG::isHexColor('ff00ff'));
+		static::assertEquals(-1, BTG::isHexColor('#fff000fff'));
+	}
+
+	/**
+	 * Test: getNestedArray
+	 */
+	public function test_getNestedArray() {
+		$nestedArray = [
+			['name' => 'Adam'],
+			['name' => 'Bob'],
+			['name' => 'Carl']
+		];
+
+		// correct
+		static::assertEquals(['name' => 'Bob'], BTG::getNestedArray($nestedArray, 'name', 'Bob'));
+		// wrong
+		static::assertEquals(null, BTG::getNestedArray($nestedArray, 'name', 'Alice'));
 	}
 
 }
